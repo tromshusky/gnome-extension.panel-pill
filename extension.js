0f0d763c45f7d0f12a889b7492561463b978ff15
tromshusky <tromshusky@gmail.com>
2025-09-28 23:51:17 +0200

    yaya

--- extension.js
+++ extension.js
@@ -1,45 +1,93 @@
-const { St, Clutter } = imports.gi;
-const Main = imports.ui.main;
-const Shell = imports.gi.Shell;
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as Main from "resource:///org/gnome/shell/ui/main.js";
+import Clutter from "gi://Clutter";
 
-const panel_height = Main.panel.height;
+// import Shell from "gi://Shell";
+// import St from "gi://St";
+
 const panel_parent = Main.panel.get_parent();
-const elem_width = Main.panel.get_children().map(child=>child.width).reduce((a,b)=>a+b);
-const min_width = elem_width + (panel_height*8);
-const screen_width = global.screen_width;
-const new_width = Math.min(min_width,screen_width);
-const new_x = (screen_width - new_width) / 2;
-const new_y = panel_height/2;
-const new_radius = panel_height/2;
+const elem_width = Main.panel.get_children().map(child => child.width).reduce((a, b) => a + b);
+const min_width = elem_width + (Main.panel.height * 8);
+const new_width = Math.min(min_width, global.screen_width);
+const new_x = (global.screen_width - new_width) / 2;
+const new_y = 4;
+const new_radius = Main.panel.height / 2;
+const panel_opacity_low = 100;
+const panel_opacity_high = 235;
+const timeout_hide = 7000;
+const timeout_almostback = 1500;
+const stretch_after_maximize = 400;
 
-class OverviewExtension {
-    constructor() {
-        this._overviewStateChangedId = null;
-    }
+const set_opacity_panel_low = () => { Main.panel.opacity = panel_opacity_low; };
+const set_opacity_panel_high = () => { Main.panel.opacity = panel_opacity_high; };
+const set_panel_reactivity = (value) => {
+    Main.panel.get_children().map(e => {
+        e.get_children().map(f => { f.first_child.reactive = value; });
+    });
+}
+
+export default class PlainExampleExtension extends Extension {
+    #mainOverviewListenerID1 = null;
+    #mainOverviewListenerID2 = null;
+    #mainPanelListenerID1 = null;
+    #windowManagerListenerID1 = null;
+
 
     enable() {
+        this.#mainPanelListenerID1 = Main.panel.connect('button-press-event', () => {
+            Main.panel.hide();
+
+            setTimeout(() => {
+                set_panel_reactivity(false);
+                set_opacity_panel_low();
+                Main.panel.show();
+            }, timeout_hide);
+
+            setTimeout(() => {
+                set_panel_reactivity(true);
+                set_opacity_panel_high();
+            }, timeout_hide + timeout_almostback);
+            return Clutter.EVENT_STOP; // Prevent further handling of the event
+        });
+        const onWindowMaximized = (wm, win) => {
+            if (win.metaWindow.get_maximized() == 3) {
+                const unmax = () => win.metaWindow.unmaximize(3);
+                const stretch = () => win.metaWindow.move_resize_frame(false, 0, 0, global.screen_width, global.screen_height);
+                unmax();
+                setTimeout(stretch, stretch_after_maximize);
+            };
+        };
+        this.#windowManagerListenerID1 = global.window_manager.connect_after('size-change', onWindowMaximized);
+
         panel_parent.x = new_x;
         panel_parent.y = new_y;
         panel_parent.width = new_width;
-        Main.panel.set_style("border-radius: " + new_radius + "px;");
-        this._overviewStateChangedId = Main.overview.connect('showing', () => {
-            Main.overview._overview.first_child.first_child.margin_top = new_y + panel_height + new_y;
+        Main.panel.opacity = panel_opacity_high;
+        const make_round = () => Main.panel.set_style("border-radius: " + new_radius + "px;");
+        setTimeout(make_round, 1000);
+        this.#mainOverviewListenerID1 = Main.overview.connect('showing', () => {
+            setTimeout(make_round, 1000);
+            Main.overview._overview.first_child.first_child.margin_top = new_y + Main.panel.height + new_y;
+        });
+        this.#mainOverviewListenerID2 = Main.overview.connect('hiding', () => {
+            setTimeout(make_round, 1000);
+            Main.overview._overview.first_child.first_child.margin_top = new_y + Main.panel.height + new_y;
         });
     }
 
     disable() {
+        Main.panel.disconnect(this.#mainPanelListenerID1);
+        this.#mainPanelListenerID1 = null;
+        global.window_manager.disconnect(this.#windowManagerListenerID1);
+        this.#windowManagerListenerID1 = null;
         panel_parent.x = 0;
         panel_parent.y = 0;
-        panel_parent.width = screen_width;
+        panel_parent.width = global.screen_width;
         Main.panel.set_style("border-radius: " + 0 + "px;");
-        if (this._overviewStateChangedId) {
-            Main.overview.disconnect(this._overviewStateChangedId);
-            this._overviewStateChangedId = null;
-        }
+        Main.overview.disconnect(this.#mainOverviewListenerID1);
+        this.#mainOverviewListenerID1 = null;
+        Main.overview.disconnect(this.#mainOverviewListenerID2);
+        this.#mainOverviewListenerID2 = null;
     }
-}
-
-function init() {
-    return new OverviewExtension();
 }
 
