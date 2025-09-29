1e7925ae278593922982e3722201e6a58d6d0b84
tromshusky <tromshusky@gmail.com>
2025-09-29 16:19:27 +0200

    adding clearTimeouts, renaming variables

--- extension.js
+++ extension.js
@@ -9,7 +9,7 @@ const panel_opacity_low = 100;
 const panel_opacity_high = 235;
 const timeout_hide = 7000;
 const timeout_almostback = 1500;
-const stretch_after_maximize = 400;
+const timeout_stretch_after_maximize = 400;
 
 const set_opacity_panel_low = () => { Main.panel.opacity = panel_opacity_low; };
 const set_opacity_panel_high = () => { Main.panel.opacity = panel_opacity_high; };
@@ -25,6 +25,22 @@ export default class PanelPillExtension extends Extension {
     #mainPanelListenerID1 = null;
     #windowManagerListenerID1 = null;
 
+    #timeoutVanishID = null;
+    #timeoutFadeinID = null;
+    #timeoutRoundnessID = null;
+    #timeoutStretchID = null;
+
+
+    makePanelRound(){
+        const new_radius = Main.panel.height / 2;
+        const make_round = () => {
+            Main.panel.set_style("border-radius: " + new_radius + "px;");
+        };
+        make_round();
+        if (this.#timeoutRoundnessID != null)
+            clearTimeout(this.#timeoutRoundnessID);
+        this.#timeoutRoundnessID = setTimeout(make_round, 1000);
+    }
 
     enable() {
         const panel_parent = Main.panel.get_parent();
@@ -33,45 +49,57 @@ export default class PanelPillExtension extends Extension {
         const new_width = Math.min(min_width, global.screen_width);
         const new_x = (global.screen_width - new_width) / 2;
         const new_y = 4;
-        const new_radius = Main.panel.height / 2;
 
         this.#mainPanelListenerID1 = Main.panel.connect('button-press-event', () => {
             Main.panel.hide();
 
-            setTimeout(() => {
+            if (this.#timeoutVanishID != null)
+                clearTimeout(this.#timeoutVanishID);
+
+            this.#timeoutVanishID = setTimeout(() => {
                 set_panel_reactivity(false);
                 set_opacity_panel_low();
                 Main.panel.show();
             }, timeout_hide);
 
-            setTimeout(() => {
+            if (this.#timeoutFadeinID != null)
+                clearTimeout(this.#timeoutFadeinID);
+
+            this.#timeoutFadeinID = setTimeout(() => {
                 set_panel_reactivity(true);
                 set_opacity_panel_high();
             }, timeout_hide + timeout_almostback);
+
             return Clutter.EVENT_STOP; // Prevent further handling of the event
         });
+
         const onWindowMaximized = (wm, win) => {
             if (win.metaWindow.get_maximized() == 3) {
-                const unmax = () => win.metaWindow.unmaximize(3);
-                const stretch = () => win.metaWindow.move_resize_frame(false, 0, 0, global.screen_width, global.screen_height);
-                unmax();
-                setTimeout(stretch, stretch_after_maximize);
+                const unmaxWindow = () => win.metaWindow.unmaximize(3);
+                const stretchWindow = () => win.metaWindow.move_resize_frame(false, 0, 0, global.screen_width, global.screen_height);
+                
+                unmaxWindow();
+                if (this.#timeoutStretchID)
+                    clearTimeout(this.#timeoutStretchID);
+                this.#timeoutStretchID = setTimeout(stretchWindow, timeout_stretch_after_maximize);
             };
         };
+
         this.#windowManagerListenerID1 = global.window_manager.connect_after('size-change', onWindowMaximized);
 
         panel_parent.x = new_x;
         panel_parent.y = new_y;
         panel_parent.width = new_width;
         Main.panel.opacity = panel_opacity_high;
-        const make_round = () => Main.panel.set_style("border-radius: " + new_radius + "px;");
-        setTimeout(make_round, 1000);
+
+
+        this.makePanelRound();
         this.#mainOverviewListenerID1 = Main.overview.connect('showing', () => {
-            setTimeout(make_round, 1000);
+            this.makePanelRound();
             Main.overview._overview.first_child.first_child.margin_top = new_y + Main.panel.height + new_y;
         });
         this.#mainOverviewListenerID2 = Main.overview.connect('hiding', () => {
-            setTimeout(make_round, 1000);
+            this.makePanelRound();
             Main.overview._overview.first_child.first_child.margin_top = new_y + Main.panel.height + new_y;
         });
     }
@@ -89,12 +117,26 @@ export default class PanelPillExtension extends Extension {
         panel_parent.y = 0;
         panel_parent.width = global.screen_width;
 
+        set_panel_reactivity(true);
+        Main.panel.opacity = 255;
         Main.panel.set_style("border-radius: " + 0 + "px;");
 
         Main.overview.disconnect(this.#mainOverviewListenerID1);
         this.#mainOverviewListenerID1 = null;
         Main.overview.disconnect(this.#mainOverviewListenerID2);
         this.#mainOverviewListenerID2 = null;
+
+        if (this.#timeoutVanishID != null)
+            clearTimeout(this.#timeoutVanishID);
+
+        if (this.#timeoutFadeinID != null)
+            clearTimeout(this.#timeoutFadeinID);
+
+        if (this.#timeoutStretchID != null)
+            clearTimeout(this.#timeoutStretchID);
+        
+        if (this.#timeoutRoundnessID != null)
+            clearTimeout(this.#timeoutRoundnessID);
     }
 }
 
