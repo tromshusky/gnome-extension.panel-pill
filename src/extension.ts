0ad7c6ad53646ff588994b9120aa9fad35f80884
tromshusky <tromshusky@github>
2025-11-08 17:54:57 +0100

    internal optimizing

--- src/extension.ts
+++ src/extension.ts
@@ -36,8 +36,7 @@ export default class PanelPillExtension extends Extension {
     }
 
     disable() {
-        this.automove.disable();
-        this.pill.disable();
+        this.automove.disable().pill.disable();
     }
 
 }
