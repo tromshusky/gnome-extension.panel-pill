7fa809ed2a0f13aa99068bde0f83acf2ab41788e
tromshusky <tromshusky@github>
2025-11-08 16:46:54 +0100

    bugfix:  Panel dimensions

--- src/panelUI.ts
+++ src/panelUI.ts
@@ -1,13 +1,13 @@
 import * as Main from "resource:///org/gnome/shell/ui/main.js";
 
 export default class panelUI {
-    static shrinkToPill(){
-        Main.layoutManager.panelBox.x = global.screen_width *  2 / 5;
-        Main.layoutManager.panelBox.width = global.screen_width / 5;
+    static shrinkToPill() {
+        Main.layoutManager.panelBox.x = global.screen_width * 3 / 8;
+        Main.layoutManager.panelBox.width = global.screen_width / 4;
         Main.layoutManager.panelBox.y = 0;
         Main.layoutManager.panelBox.height = 4;
     }
-    static expandToNormal(){
+    static expandToNormal() {
         Main.layoutManager.panelBox.x = 0;
         Main.layoutManager.panelBox.y = 0;
         Main.layoutManager.panelBox.width = global.screen_width;
