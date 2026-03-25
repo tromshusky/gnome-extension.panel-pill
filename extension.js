fa55f5c5b6a57802ba05ef0bd455031e5cc1f672
tromshusky <tromshusky@github>
2026-03-25 11:08:39 +0100

    one step further

--- extension.js
+++ extension.js
@@ -1,10 +1,13 @@
+// @ts-ignore
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+// @ts-ignore
 import * as Main from "resource:///org/gnome/shell/ui/main.js";
-import Clutter from "gi://Clutter";
-import Gio from 'gi://Gio';
+import FeatureManager from './gnome-extensions-utils/FeatureManager';
+// import Clutter from "gi://Clutter";
+// import Gio from 'gi://Gio';
 
 // import Shell from "gi://Shell";
-import St from "gi://St";
+// import St from "gi://St";
 
 const SETTING_ISLANDS = "islands";
 const SETTING_PANEL_GAP = "panel-gap";
@@ -12,66 +15,9 @@ const SETTING_WINDOW_GAP = "window-gap";
 const SETTING_SQUARE_CORNERS = "square-corners";
 const SETTING_HIDE_BUTTON = "hide-button";
 const SETTING_EASY_DOCK = "easy-dock";
+// @ts-ignore
+const global = global;
 
-
-class ManagedFeature {
-    #featureManager;
-    constructor(featureManager) {
-        this.#featureManager = featureManager;
-    }
-    enable() {
-        return this.#featureManager.enable(this);
-    }
-    disable() {
-        return this.#featureManager.disable(this);
-    }
-}
-
-/** @typedef RegisteredFeatureData @type {{onEnable: Function, onDisable: Function, eventListeners: Array<EventListenerData>}}  */
-/** @typedef EventListenerData @type {{connectable: Connectable, event: String, callback: Function}} */
-/** @typedef Connectable @type {{connect: ()=>number, disconnect:(id: number) => void}} */
-/** @typedef ActiveEventListener @type {{id: number, connectable: Connectable}} */
-
-class FeatureManager {
-    /** @type {Map<ManagedFeature,RegisteredFeatureData>} */
-    #features = new Map();
-    /** @type {Map<ManagedFeature,Array<ActiveEventListener>>} */
-    #activeEventListeners = new Map();
-
-    new(onEnable, onDisable, eventListeners) {
-        const newFeature = new ManagedFeature(this);
-        this.#features.set(newFeature, { onEnable, onDisable, eventListeners });
-        return newFeature;
-    }
-
-    enable(featureHandle) {
-        this.#disconnect(featureHandle);
-        const registeredFeature = this.#features.get(featureHandle);
-        if (registeredFeature) {
-            registeredFeature.onEnable();
-            const newEventListeners = registeredFeature.eventListeners.map(elData => { return { id: elData.connectable.connect(elData.event, elData.callback), connectable: elData.connectable } });
-            this.#activeEventListeners.set(featureHandle, newEventListeners);
-        }
-    }
-
-    #disconnect(featureHandle) {
-        const disconnectAnswer = this.#activeEventListeners.get(featureHandle)?.map(eventListener => eventListener.connectable.disconnect(eventListener.id));
-        this.#activeEventListeners.set(featureHandle, null);
-        return disconnectAnswer;
-    }
-
-    disable(featureHandle) {
-        this.#disconnect(featureHandle);
-        const registeredFeature = this.#features.get(featureHandle);
-        if (registeredFeature) {
-            registeredFeature.onDisable();
-        }
-    }
-
-    disableAll() {
-        return this.#features.forEach((registeredFeature, featureHandle) => this.disable(featureHandle));
-    }
-}
 
 export default class PanelPillExtension extends Extension {
     #featureManager = null;
@@ -86,9 +32,16 @@ export default class PanelPillExtension extends Extension {
         this.featureManager.disableAll();
         this.#featureManager = undefined;
 
+
         delete global._panelpill;
     }
 
+    /** @returns {{ get_boolean:(name: string) => boolean }} */
+    settings() {
+        return super.settings();
+    }
+
+    /** @returns {FeatureManager} */
     get featureManager() {
         return this.#featureManager ??= new FeatureManager();
     }
@@ -101,14 +54,50 @@ export default class PanelPillExtension extends Extension {
         return this.settings().get_boolean(SETTING_PANEL_GAP) ? this.PANEL_GAP : 0;
     }
 
+    setPanelPlacement() {
+        Main.layoutManager.panelBox.y = global.screen_height - this.windowGap();
+        Main.layoutManager.panelBox.x = this.panelGap();
+        Main.layoutManager.panelBox.width = global.screen_width - this.panelGap() - this.panelGap();
+        Main.panel.translation_y = this.panelGap() - Main.layoutManager.panelBox.y;
+    }
+    resetPanelPlacement() {
+        Main.layoutManager.panelBox.y = 0;
+        Main.layoutManager.panelBox.x = 0;
+        Main.layoutManager.panelBox.width = global.screen_width;
+        Main.panel.translation_y = 0;
+    }
+
+    setPanelStyle() {
+        Main.panel.style = "background-color: transparent;";
+    }
+
+    resetPanelStyle() {
+        Main.panel.style = null;
+    }
+
+    setPanelReactivity() {
+        Main.panel.reactive = false;
+    }
+    resetPanelReactivity() {
+        Main.panel.reactive = true;
+    }
+
+    setIslandsStyle() {
+        Main.panel.get_children().map(c => c.style = "background-color: rgba(100,100,100,100); border-radius:999;")
+    }
+
     getIslandFeature() {
-        const setPanelPlacement = () => {
-            Main.layoutManager.panelBox.y = global.screen_height - this.windowGap();
-            Main.layoutManager.panelBox.x = this.panelGap();
-            Main.layoutManager.panelBox.width = global.screen_width - this.panelGap() - this.panelGap();
-            Main.panel.translation_y = this.panelGap() - Main.layoutManager.panelBox.y;
+        const onEnable = () => undefined;
+        const onDisable = () => {
         };
 
+        const el1 = {
+            connectable: Main.panel,
+            event: "notify",
+            callback: () => this.setPanelPlacement()
+        };
+
+        const asd = this.featureManager.new(onEnable, onDisable, [el1]);
 
     }
 }
